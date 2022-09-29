4dfa524f75d540e0dd9b2acf2434a4eed47a252a
Olgun Özoktaş <olgun_ozoktas@hotmail.com>
2022-09-30 00:25:32 +0300

    Contract Staff Dashboard

--- resources/views/contract_staff_dashboard.blade.php
+++ resources/views/contract_staff_dashboard.blade.php
@@ -5,14 +5,18 @@
 @endpush
 
 @section('content')
-    <div class="grid grid-cols-4">
-        <div class="py-2 px-4 bg-white shadow-2xl">
+    <div class="flex flex-col justify-center gap-2 py-2 px-4 bg-white shadow-2xl rounded-lg border border-gray-200">
+        <div class="flex flex justify-between items-center">
             <p class="text-lg font-black">Application
                 Status: {{ ucfirst($application->status) }}</p>
-
-            @if($application->isPending())
-                <p class="text-red-700 font-bold">{{ $application->notes }}</p>
+            @if($application->isRejected())
+                <a href="{{ route('applications.create') }}" class="py-2 px-4 bg-blue-500 text-white rounded-2xl">Create New Application</a>
             @endif
         </div>
+
+        @if($application->isRejected())
+            <p class="text-red-700 font-bold">{{ $application->notes }}</p>
+
+        @endif
     </div>
 @endsection
